ccef9a1a37341680feef1627f9298d076e52c7e6
domaincrisman <cmanzanore@gmail.com>
2016-12-01 23:26:24 +0100

    Creating Tags

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,30 @@
+class Admin::TagsController < Admin::ApplicationController
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    tags_parms[:name].split(',').map do |n|
+      Tag.new(name: n).save
+    end
+    redirect_to new_admin_tag_url, notice: 'Tag was successfully created'
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def tags_parms
+    params.require(:tag).permit(:id, :name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,11 @@
   Rails.application.routes.draw do
   
-
-  namespace :admin do
-    get 'posts/index'
-  end
-
-  namespace :admin do
-    get 'posts/new'
-  end
-
-  namespace :admin do
-    get 'posts/create'
-  end
-
-  namespace :admin do
-    get 'posts/edit'
-  end
-
-  namespace :admin do
-    get 'posts/update'
-  end
-
-  namespace :admin do
-    get 'posts/show'
-  end
-
-  namespace :admin do
-    get 'posts/destroy'
-  end
-
   get '/login' => 'admin/sessions#new'
   get '/logout' => 'admin/sessions#destroy'
 
   namespace :admin do
     resources :posts
+    resources :tags, except: [:index]
     resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
