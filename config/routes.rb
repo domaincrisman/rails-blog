78130af5c2d6524c36a62b42c3f702a7e74f945f
domaincrisman <cmanzanore@gmail.com>
2016-12-03 00:27:27 +0100

    Creating Comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@
 
   namespace :admin do
     resources :posts
+    resources :comments, only: [:index, :update, :destroy]
     resources :tags, except: [:index]
     resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
