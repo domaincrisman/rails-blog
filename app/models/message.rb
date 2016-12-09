655b4b56eedd17a0a6831c7bd434223a683134d1
domaincrisman <cmanzanore@gmail.com>
2016-12-09 18:48:01 +0100

    Sending Messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :visitor
+
+  
+
+  def self.matching_fullname_or_message params
+    joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "%#{params}%", "%#{params}%")
+  end
+
+  def mark_read
+    update(status: true) if status == false
+  end
 end
